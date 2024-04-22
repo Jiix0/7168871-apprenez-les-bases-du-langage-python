dab137210749d9af2789d2cff0a7bb82a668d4d3
Jiix0 <167770843+Jiix0@users.noreply.github.com>
2024-04-22 14:00:44 +0200

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,15 @@
 # Écrivez votre code ici !
+'''Créez une liste nommée fruits contenant les éléments "pomme", "banane" et "orange".
+Ajoutez "kiwi" à la liste fruits.
+Supprimez "orange" de la liste fruits.
+Modifiez le deuxième élément de la liste fruits en "ananas".
+Affichez la longueur de la liste fruits.
+Triez la liste fruits par ordre alphabétique
+Affichez la liste. (N'oubliez pas d'afficher la liste pour que les tests passent)'''
+
+
+fruits = ["pomme","banana","orange"]
+fruits.append("kiwi")
+fruits.remove("orange")
+fruits[1] = "ananas"
+print(len(fruits))
